ce3bb5cb9796dc845123900aee44252be2de71bf
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-12-21 01:43:58 +0100

    override user filer on users connection

--- src/user/dto/users.input.ts
+++ src/user/dto/users.input.ts
@@ -1,6 +1,10 @@
-import { InputType, ArgsType } from '@nestjs/graphql';
+import { InputType, ArgsType, Field } from '@nestjs/graphql';
 import { ConnectionInput } from 'src/common/pagination/dto/connection.input';
+import { UserFilterInput } from './user-filter.input';
 
 @InputType()
 @ArgsType()
-export class UsersInput extends ConnectionInput {}
+export class UsersInput extends ConnectionInput {
+  @Field(() => UserFilterInput, { nullable: true })
+  filter?: UserFilterInput = new UserFilterInput();
+}
