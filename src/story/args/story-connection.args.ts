bb7f861df82c1f9b9994090f43c006a4b36e3826
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-12-23 00:35:13 +0100

    fix story connection args

--- src/story/args/story-connection.args.ts
+++ src/story/args/story-connection.args.ts
@@ -1,10 +1,9 @@
 import { ArgsType, Field } from '@nestjs/graphql';
-import { CommentFilterInput } from 'src/comment/dto/comment-filter.input';
 import { ConnectionArgs } from 'src/common/pagination/args/connection.args';
 import { StoryFilterInput } from '../dto/story-filter.input';
 
 @ArgsType()
 export class StoryConnectionArgs extends ConnectionArgs {
-  @Field(() => CommentFilterInput, { nullable: true })
+  @Field(() => StoryFilterInput, { nullable: true })
   filter?: StoryFilterInput = new StoryFilterInput();
 }
