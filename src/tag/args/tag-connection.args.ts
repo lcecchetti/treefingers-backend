0d03e728fc889916b9f53666d4681e73094c1c86
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-01-22 16:12:01 +0100

    add tag stories count sort order

--- src/tag/args/tag-connection.args.ts
+++ src/tag/args/tag-connection.args.ts
@@ -1,9 +1,13 @@
 import { ArgsType, Field } from '@nestjs/graphql';
 import { ConnectionArgs } from 'src/query/args/connection.args';
 import { FilterTagInput } from '../inputs/filter-tag.input';
+import { SortTagInput } from '../inputs/sort-tag.input';
 
 @ArgsType()
 export class TagConnectionArgs extends ConnectionArgs {
   @Field(() => FilterTagInput, { nullable: true })
   filter?: FilterTagInput;
+
+  @Field(() => SortTagInput, { nullable: true })
+  sort?: SortTagInput = new SortTagInput();
 }
