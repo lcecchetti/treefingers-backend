5b9bf2892dd0f03bea0d43425c62d6496e9f1cc9
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-05-01 14:56:30 +0200

    set max limit to paginated queries

--- src/pagination/args/connection.args.ts
+++ src/pagination/args/connection.args.ts
@@ -1,16 +1,18 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
-import { IsOptional, Min } from 'class-validator';
+import { IsOptional, Max, Min } from 'class-validator';
 
 @ArgsType()
 export class ConnectionArgs {
   @Field(() => Int, { nullable: true })
   @IsOptional()
   @Min(1)
+  @Max(10)
   readonly first?: number;
 
   @Field(() => Int, { nullable: true })
   @IsOptional()
   @Min(1)
+  @Max(10)
   readonly last?: number;
 
   @Field({ nullable: true })
