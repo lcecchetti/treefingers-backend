36b1e3314c86c106235e888d66265870abc5aa12
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-05-09 00:12:23 +0200

    return current user data on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -66,6 +66,7 @@ export class AuthService {
 
     return {
       token: this.jwtService.sign(payload),
+      currentUser: user,
     };
   }
 
--- src/auth/payloads/login.payload.ts
+++ src/auth/payloads/login.payload.ts
@@ -1,7 +1,11 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { CurrentUser } from '../dto/current-user.dto';
 
 @ObjectType()
 export class LoginPayload {
   @Field()
   token: string;
+
+  @Field(() => CurrentUser)
+  currentUser: CurrentUser;
 }
