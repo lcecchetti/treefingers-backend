c87db0a11a9863d4263da35ead21608a95b456ee
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-05-17 01:44:44 +0200

    implement filtering with query builder

--- src/query/query.service.ts
+++ src/query/query.service.ts
@@ -1,24 +1,114 @@
 import { Injectable } from '@nestjs/common';
-import { Repository, SelectQueryBuilder } from 'typeorm';
+import {
+  Brackets,
+  Repository,
+  SelectQueryBuilder,
+  WhereExpressionBuilder,
+} from 'typeorm';
 import { FilterInput } from './inputs/filter.input';
 import { SortInput } from './inputs/sort.input';
 
+const operatorsMap = {
+  eq: '=',
+  neq: '!=',
+  lt: '<',
+  lte: '<=',
+  gt: '>',
+  gte: '>=',
+  in: 'IN',
+  nin: 'NOT IN',
+  like: 'LIKE',
+  ilike: 'ILIKE',
+};
+
 @Injectable()
 export class QueryService<Entity> {
-  private queryBuilder: SelectQueryBuilder<Entity>;
-
-  addFilter(filter: FilterInput): void {
-    return;
-  }
-
-  addSort(sort: SortInput): void {
-    const orderBy = {};
-
-    Object.keys(sort).forEach((key) => {
-      orderBy[key] = sort[key];
+  addCondition(
+    where: WhereExpressionBuilder,
+    filter: FilterInput,
+    field: string,
+    paramId: number,
+  ): WhereExpressionBuilder {
+    Object.entries(filter).map(([operator, value]) => {
+      switch (operator) {
+        case 'eq':
+        case 'neq':
+        case 'lt':
+        case 'lte':
+        case 'gt':
+        case 'gte':
+          where.andWhere(`${field} ${operatorsMap[operator]} :${paramId}`, {
+            [paramId]: value,
+          });
+          break;
+        case 'like':
+        case 'ilike':
+          where.andWhere(`${field} ${operatorsMap[operator]} %:${paramId}%`, {
+            [paramId]: value,
+          });
+          break;
+        case 'in':
+        case 'nin':
+          where.andWhere(
+            `${field} ${operatorsMap[operator]} (:...${paramId})`,
+            {
+              [paramId]: value,
+            },
+          );
+          break;
+      }
     });
 
-    this.queryBuilder.orderBy(orderBy);
+    return where;
+  }
+
+  addFilter(
+    where: WhereExpressionBuilder,
+    filter: FilterInput,
+    field = '',
+    paramId = 0,
+  ) {
+    if (!filter) {
+      return where;
+    }
+
+    Object.entries(filter).map(([key, value]) => {
+      switch (key) {
+        case 'or':
+        case 'and':
+          where.andWhere(
+            new Brackets((qb) =>
+              value.forEach((subFilter) => {
+                qb[`${key}Where`](
+                  new Brackets((orQb) =>
+                    this.addFilter(orQb, subFilter, key, paramId++),
+                  ),
+                );
+              }),
+            ),
+          );
+          break;
+        default:
+          if (operatorsMap[key]) {
+            this.addCondition(where, filter, field, paramId++);
+          } else {
+            this.addFilter(where, value, key, paramId++);
+          }
+      }
+    });
+  }
+
+  addSort(
+    queryBuilder: SelectQueryBuilder<Entity>,
+    sort: SortInput,
+  ): SelectQueryBuilder<Entity> {
+    const orderBy = {};
+
+    Object.entries(sort).map(([key, value]) => {
+      orderBy[key] = value;
+    });
+
+    return queryBuilder.orderBy(orderBy);
   }
 
   prepareQueryBuilder(
@@ -26,11 +116,13 @@ export class QueryService<Entity> {
     filter: FilterInput,
     sort: SortInput = new SortInput(),
   ): SelectQueryBuilder<Entity> {
-    this.queryBuilder = repository.createQueryBuilder();
+    const queryBuilder = repository.createQueryBuilder();
+    queryBuilder.where(
+      new Brackets((qb) => (filter) => this.addFilter(qb, filter)),
+    );
+    this.addFilter(queryBuilder, filter);
+    this.addSort(queryBuilder, sort);
 
-    this.addFilter(filter);
-    this.addSort(sort);
-
-    return this.queryBuilder;
+    return queryBuilder;
   }
 }
