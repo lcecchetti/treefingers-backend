27269cab1512632b7d0e48dd475e14003f4b5820
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-06-01 03:46:07 +0200

    fix mikro orm paths

--- migrations/Migration20220601014449.ts
+++ migrations/Migration20220601014449.ts
@@ -1,6 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20220531192601 extends Migration {
+export class Migration20220601014449 extends Migration {
 
   async up(): Promise<void> {
     this.addSql('create table "user" ("id" serial primary key, "email" varchar(255) not null, "password" varchar(255) not null, "username" varchar(255) not null, "bio" text null, "is_active" boolean not null default false, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
@@ -8,11 +8,6 @@ export class Migration20220531192601 extends Migration {
     this.addSql('alter table "user" add constraint "user_username_unique" unique ("username");');
     this.addSql('create index "user_bio_index" on "user" ("bio");');
     this.addSql('create index "user_is_active_index" on "user" ("is_active");');
-
-    this.addSql('create table "followership" ("id" serial primary key, "followed_id" int not null, "follower_id" int not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
-    this.addSql('create index "followership_followed_id_index" on "followership" ("followed_id");');
-    this.addSql('create index "followership_follower_id_index" on "followership" ("follower_id");');
-    this.addSql('alter table "followership" add constraint "followership_followed_id_follower_id_unique" unique ("followed_id", "follower_id");');
 
     this.addSql('create table "forest" ("id" serial primary key, "name" varchar(255) not null, "about" text not null, "founder_id" int not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
     this.addSql('alter table "forest" add constraint "forest_name_unique" unique ("name");');
@@ -34,6 +29,11 @@ export class Migration20220531192601 extends Migration {
     this.addSql('create index "story_tags_index" on "story" ("tags");');
     this.addSql('create index "story_forest_id_index" on "story" ("forest_id");');
 
+    this.addSql('create table "followership" ("id" serial primary key, "followed_id" int not null, "follower_id" int not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
+    this.addSql('create index "followership_followed_id_index" on "followership" ("followed_id");');
+    this.addSql('create index "followership_follower_id_index" on "followership" ("follower_id");');
+    this.addSql('alter table "followership" add constraint "followership_followed_id_follower_id_unique" unique ("followed_id", "follower_id");');
+
     this.addSql('create table "comment" ("id" serial primary key, "content" text not null, "user_id" int not null, "story_id" int null, "forest_id" int null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, constraint comment_check check (story_id IS NOT NULL OR forest_id IS NOT NULL));');
     this.addSql('create index "comment_content_index" on "comment" ("content");');
     this.addSql('create index "comment_user_id_index" on "comment" ("user_id");');
@@ -48,9 +48,6 @@ export class Migration20220531192601 extends Migration {
     this.addSql('create index "like_user_id_comment_id_index" on "like" ("user_id", "comment_id");');
     this.addSql('alter table "like" add constraint "like_user_id_story_id_comment_id_unique" unique ("user_id", "story_id", "comment_id");');
 
-    this.addSql('alter table "followership" add constraint "followership_followed_id_foreign" foreign key ("followed_id") references "user" ("id") on update cascade on delete cascade;');
-    this.addSql('alter table "followership" add constraint "followership_follower_id_foreign" foreign key ("follower_id") references "user" ("id") on update cascade on delete cascade;');
-
     this.addSql('alter table "forest" add constraint "forest_founder_id_foreign" foreign key ("founder_id") references "user" ("id") on update cascade;');
 
     this.addSql('alter table "membership" add constraint "membership_forest_id_foreign" foreign key ("forest_id") references "forest" ("id") on update cascade on delete cascade;');
@@ -60,6 +57,9 @@ export class Migration20220531192601 extends Migration {
     this.addSql('alter table "story" add constraint "story_parent_id_foreign" foreign key ("parent_id") references "story" ("id") on update cascade on delete cascade;');
     this.addSql('alter table "story" add constraint "story_root_id_foreign" foreign key ("root_id") references "story" ("id") on update cascade on delete cascade;');
     this.addSql('alter table "story" add constraint "story_forest_id_foreign" foreign key ("forest_id") references "forest" ("id") on update cascade on delete set null;');
+
+    this.addSql('alter table "followership" add constraint "followership_followed_id_foreign" foreign key ("followed_id") references "user" ("id") on update cascade on delete cascade;');
+    this.addSql('alter table "followership" add constraint "followership_follower_id_foreign" foreign key ("follower_id") references "user" ("id") on update cascade on delete cascade;');
 
     this.addSql('alter table "comment" add constraint "comment_user_id_foreign" foreign key ("user_id") references "user" ("id") on update cascade;');
     this.addSql('alter table "comment" add constraint "comment_story_id_foreign" foreign key ("story_id") references "story" ("id") on update cascade on delete cascade;');
