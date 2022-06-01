264cd0d249a46b94f3f17f5110a6c218a3cfeac1
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-06-01 11:43:16 +0200

    fix edit profile form

--- src/user/inputs/edit-user.input.ts
+++ src/user/inputs/edit-user.input.ts
@@ -1,11 +1,17 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
-import { MaxLength, MinLength, ValidateNested } from 'class-validator';
+import {
+  IsOptional,
+  MaxLength,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
 
 @InputType()
 export class EditUserDataInput {
   @Field({ nullable: true })
   @MinLength(10)
+  @IsOptional()
   password?: string;
 
   @Field({ nullable: true })
