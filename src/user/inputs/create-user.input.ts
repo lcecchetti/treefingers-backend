c18ba97ac11bcc9d3a593f4981f25ffab624f0d1
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-07-27 17:15:48 +0200

    avoid edit user input code duplication

--- src/user/inputs/create-user.input.ts
+++ src/user/inputs/create-user.input.ts
@@ -1,32 +1,18 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
-import {
-  IsEmail,
-  Length,
-  Matches,
-  MaxLength,
-  MinLength,
-  ValidateNested,
-} from 'class-validator';
+import { IsEmail, Length, Matches, ValidateNested } from 'class-validator';
+import { EditUserDataInput } from './edit-user.input';
 
 @InputType()
-export class CreateUserInputData {
+export class CreateUserInputData extends EditUserDataInput {
   @Field()
   @IsEmail()
   email: string;
 
   @Field()
-  @MinLength(10)
-  password: string;
-
-  @Field()
   @Length(3, 20)
   @Matches(/^[a-zA-Z0-9-_]+$/)
   username: string;
-
-  @Field({ nullable: true })
-  @MaxLength(4096)
-  bio?: string;
 }
 
 @InputType()
