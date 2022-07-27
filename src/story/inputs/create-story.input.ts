91b2ccc2f7522c410320a1c2215d4c151d647593
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-07-27 17:07:14 +0200

    avoid edit story input code duplication

--- src/story/inputs/create-story.input.ts
+++ src/story/inputs/create-story.input.ts
@@ -1,39 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
-import {
-  ArrayMaxSize,
-  ArrayUnique,
-  Matches,
-  MaxLength,
-  MinLength,
-  ValidateNested,
-} from 'class-validator';
+import { ValidateNested } from 'class-validator';
 import { EncodedID } from '../../common/scalars/encoded-id.scalar';
+import { EditStoryDataInput } from './edit-story.input';
 
 @InputType()
-export class CreateStoryDataInput {
-  @Field()
-  @MaxLength(64)
-  title: string;
-
-  @Field()
-  @MaxLength(4096)
-  content: string;
-
+export class CreateStoryDataInput extends EditStoryDataInput {
   @Field(() => EncodedID, { nullable: true })
   parent?: number;
-
-  @Field(() => [String], { nullable: true })
-  @MaxLength(16, {
-    each: true,
-  })
-  @MinLength(2, {
-    each: true,
-  })
-  @Matches(/^[a-zA-Z0-9_]*$/, { each: true })
-  @ArrayMaxSize(5)
-  @ArrayUnique()
-  tags?: string[];
 
   @Field(() => EncodedID, { nullable: true })
   forest?: number;
