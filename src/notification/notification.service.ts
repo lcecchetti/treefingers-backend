deb66b0e36e16566ee63c40c9dfbe83e250be411
Luca Cecchetti <luca.cecchetti07@gmail.com>
2026-08-01 09:14:07 +0100

    return 404 instead of crashing on a nonexistent id

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Notification } from './notification.entity';
 import { NotificationConnectionArgs } from './args/notification-connection.args';
 import { NotificationConnection } from './dto/notification-connection.dto';
@@ -64,6 +68,10 @@ export class NotificationService {
 
   async read(id: number, currentUser: CurrentUser): Promise<Notification> {
     const notification = await this.findById(id);
+
+    if (!notification) {
+      throw new NotFoundException('This notification does not exist.');
+    }
 
     if (notification.user.id !== currentUser.id) {
       throw new UnauthorizedException(
